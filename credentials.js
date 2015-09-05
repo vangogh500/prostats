77279bfccf9be968ab35699532347cfc3d7ab8c6
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-09-05 09:31:14 -0400

    test

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
 module.exports = {
 	mongo: {
-		connectionString: process.env.OPENSHIFT_MONGO_DB_URL
+		connectionString: 'mongodb://' + process.env.OPENSHIFT_MONGO_DB_USERNAME + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST+':'+process.env.OPENSHIFT_MONGODB_DB_PORT+'/'
 	}
 };
