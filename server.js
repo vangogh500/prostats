683ee32beb74c1053f2e124c09ca922e1156a9b7
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-09-07 14:15:06 -0400

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var credentials = require('./credentials.js');
 var fs      = require('fs');
-var matches = require('./lib/esports/na-lcs/matches.js');
+var teams = require('./lib/esports/na-lcs/teams.js');
 
 
 /**
@@ -168,3 +168,4 @@ var SampleApp = function() {
 var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
+teams.get();
