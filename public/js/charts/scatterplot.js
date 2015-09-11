4a63ed197154be304a1e4d80458f5c7cf9e3dade
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-09-11 19:55:32 -0400

    updated scatter plot

--- public/js/charts/scatterplot.js
+++ public/js/charts/scatterplot.js
@@ -1,6 +1,6 @@
-var margin = {top: 20, right: 20, bottom:30, left: 50};
+var margin = {top: 20, right: 20, bottom:60, left: 50};
 var width = 800 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
+var height = 520 - margin.top - margin.bottom;
 
 var colors = ["#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#FFEB45"];
 
@@ -26,7 +26,11 @@ var y = d3.scale.linear().range([height,0]);
 var xAxis = d3.svg.axis().scale(x)
 	.orient("bottom")
 	.ticks(5)
-	.tickFormat(function(d,i){ return Math.floor(i/2+1) + "-" + (i%2+1)});
+	.tickFormat(function(d,i){ 
+		var lbl = Math.floor(i/2+1) + "-" + (i%2+1);
+		if(lbl == "10-1") return "tiebreaker";
+		else return lbl;
+	});
 var yAxis = d3.svg.axis().scale(y).orient("left").ticks(4);
 
 function getDate(event) {
@@ -48,6 +52,7 @@ d3.json("../../data/elo.json", function(err,dataset) {
 			.attr("height", height + margin.top + margin.bottom)
 			.append("g")
 				.attr("transform", "translate(" + margin.left +"," + margin.top + ")");
+				
 	var defs = svg.append('defs');
 	var filter = defs.append('filter')
 		.attr('id', 'shadow')
@@ -69,7 +74,7 @@ d3.json("../../data/elo.json", function(err,dataset) {
 	
 	
 	x.domain(d3.range(example.length));
-	y.domain([300, d3.max(dataset, function(d) {return d3.max(d.events, function(d) {return d.elo}); })]);
+	y.domain([300, d3.max(dataset, function(d) {return d3.max(d.events, function(d) {return d.elo}) + 100; })]);
 	
 	//grid
 	svg.append("g")
@@ -100,8 +105,16 @@ d3.json("../../data/elo.json", function(err,dataset) {
     	.attr("transform", "rotate(-90)")
     	.text("elo rating");
     //axis
-    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
+    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis)
+    	.selectAll("text")
+    	.style("text-anchor", "end")
+    	.attr('dx', '0em')
+    	.attr('dy', '0.30em')
+    	.attr("transform", "rotate(-40)");
+    
 	svg.append("g").attr("class", "y axis").call(yAxis);
+
+	var tooltip = d3.select('.main-chart').append('div').attr('id', 'tooltip').style('opacity', 0);
 
 	dataset.forEach(function(team, idx) {
 		var data = team.events;
@@ -119,10 +132,16 @@ d3.json("../../data/elo.json", function(err,dataset) {
 				.attr('height', 20)
 				.attr('width', 20)
 				.attr('xlink:href', team.logoUrl);			
-		svg.append("path")
+		var path = svg.append("path")
 			.attr("class", "line")
 			.attr("d", line(data))
-   			.style('stroke', colors[idx]);
+   			.style('stroke', colors[idx])
+   			.on("mouseover", function() {
+   				d3.select(this).style("stroke-width", 4);
+   			})
+   			.on("mouseout", function() {
+   				d3.select(this).style("stroke-width", 2);
+   			});
    	
    		// plot points
 		svg.selectAll("dot")
@@ -133,14 +152,27 @@ d3.json("../../data/elo.json", function(err,dataset) {
 				.attr("cx", function(d, i) { return x(i) + (width/data.length)/2; })
 				.attr("cy", function(d) { return y(d.elo); })
 				.attr("fill", "url(#"+ team.acronym +")")
-				.on("mouseover", function() {
-					d3.select(this).attr("filter", 'url(#shadow)');
+				.attr("opacity", 0.7)
+				.attr("cursor", "pointer")
+				.on("mouseover", function(d) {
+					d3.select(this).attr("opacity", 1);
+					path.style("stroke-width", 4);
 					var xPos = parseFloat(d3.select(this).attr("cx"));
 					var yPos = parseFloat(d3.select(this).attr("cy"));
 					this.parentNode.appendChild(this);
+					tooltip.transition()		
+						.duration(400)		
+						.style("opacity", 1);		
+					tooltip.html(d.acronym + "<br/>" + (new Date(d.gameCreation).getMonth() +1) + "/" + new Date(d.gameCreation).getDate())	
+						.style("left", (xPos) + "px")		
+						.style("top", (yPos - 20) + "px");	
 				})
 				.on("mouseout", function() {
-					d3.select(this).attr("filter", null);
+					path.style("stroke-width", 2);
+					d3.select(this).attr("filter", null).attr("opacity", 0.7);
+					tooltip.transition()		
+						.duration(400)		
+						.style("opacity", 0);	
 				})
 				.text(function(d) {
 					return "test";
