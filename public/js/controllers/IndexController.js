f03dbb92b79e1352af9fc95eb964823a00610659
Kai Sano Matsuda <kaisanomatsuda@1x-vl915-72-19-127-79.wireless.umass.edu>
2015-09-16 12:08:04 -0400

    begin implementing select in app

--- public/js/controllers/IndexController.js
+++ public/js/controllers/IndexController.js
@@ -1,4 +1,10 @@
 app.controller('IndexController', function($scope){
+	$scope.choice;
+	$scope.choices = [
+		{ name: 'Kills', detail: 'Avg Kills' },
+		{ name: 'Deaths', detail: 'Avg Deaths' },
+		{ name: 'Assists', detail: 'Avg Assists'}
+	];
 	d3.json('data/elo.json', function(err, data) {
 		$scope.data = data;
 		$scope.$apply();
