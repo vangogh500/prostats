faa1c37ec83cd2fee1085c4ce6f5867e5852b832
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-09-18 18:17:14 -0400

    implemented category selector

--- public/js/controllers/IndexController.js
+++ public/js/controllers/IndexController.js
@@ -1,12 +1,29 @@
 app.controller('IndexController', function($scope){
-	$scope.choice;
+	$scope.choice = {};
 	$scope.choices = [
-		{ name: 'Kills', detail: 'Avg Kills' },
-		{ name: 'Deaths', detail: 'Avg Deaths' },
-		{ name: 'Assists', detail: 'Avg Assists'}
+		{ name: 'Elo', detail: 'elo ratings by game', key: 'elo', yLabel: 'elo rating'},
+		{ name: 'Win Rate', detail: 'win rate by game', key: 'win', yLabel: 'win %'},
+		{ name: 'First Blood Rate', detail: "# of games where first blood was secured / # of games", key: 'firstBlood', yLabel: 'fb %'},
+		{ name: 'Avg. Dragon Kills', detail: '# of dragons secured / # of games', key: 'dragonKills', yLabel: 'dragon kills' },
+		{ name: 'First Dragons Rate', detail: "# of games where first dragon was secured / # of games", key: 'firstDragon', yLabel: 'first dragon %'},
+		{ name: 'Avg. Baron Kills', detail: '# of barons secured / # of games', key: 'baronKills', yLabel: 'baron kills' },
+		{ name: 'Avg. First Barons', detail: "# of games where first baron was secured / # of games", key: 'firstBaron', yLabel: 'first baron %'},
+		{ name: 'Avg. Tower Kills', detail: "# of towers destroyed / # of games (includes Azir's passive)", key: 'towerKills', yLabel: 'tower kills'},
+		{ name: 'Avg. First Towers', detail: "# of games where first tower was secured / # of games", key: 'firstTower', yLabel: 'first tower rate'},
+		{ name: 'Avg. Inhibitor Kills', detail: "# of inhibitors destroyed / # of games", key: 'inhibitorKills', yLabel: 'inhibitor kills'},
+		{ name: 'Avg. First Inhibitor', detail: "# of games where first inhibitor was secured / # of games", key: 'firstInhibitor', yLabel: 'first inhibitor %'}
 	];
-	d3.json('data/elo.json', function(err, data) {
-		$scope.data = data;
-		$scope.$apply();
+	function update(id, yLabel) {
+		d3.json('http://lolpro-vangogh500.rhcloud.com/api/timeline/' + id).header("Content-Type", "application/json").send("GET",function(err, data) {
+			$scope.data = {
+				data: data,
+				yLabel: yLabel
+			};
+			$scope.$apply();
+		});
+	}
+	
+	$scope.$watch('choice.selected', function(chosen) {
+		if(chosen) update(chosen.key, chosen.yLabel);
 	});
 });
--- public/js/directives/scatterchart.js
+++ public/js/directives/scatterchart.js
@@ -19,6 +19,7 @@ app.directive('scatterChart', function(){
 			.append('g')
 				.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 		
+		var labels = svg.append('g').attr('class', 'labels');
 		// add selector menu
 		var selector = d3.select(element[0]).append('div').attr('id','team-selector');
 		selector.append('h3').text('2015 Spring Split Teams');
@@ -26,6 +27,9 @@ app.directive('scatterChart', function(){
 		
 		// adds defs for filters
 		var defs = svg.append('defs');
+		
+		var lineContainer = svg.append('g').attr('class', 'line-container');
+		var plotContainer = svg.append('g').attr('class', 'plot-container');
 		
 		// defines x and y
 		var x = d3.scale.ordinal().rangeRoundBands([0,width]);
@@ -78,13 +82,37 @@ app.directive('scatterChart', function(){
 			.tickFormat("")
 		);
 		
-		
-		scope.$watch('ngModel', function(data) {
+		var xLabel = labels.append("text")
+			.attr("opacity", 0)
+			.attr("class", "xLabel")
+			.attr("text-anchor", "end")
+			.attr("x", width)
+			.attr("y", height - 6)
+			.text("");
+		var yLabel = labels.append("text")
+			.attr("opacity", 0)
+			.attr("class", "yLabel")
+			.attr("text-anchor", "end")
+			.attr("y", 6)
+			.attr("dy", ".75em")
+			.attr("transform", "rotate(-90)")
+			.text("")
+			.style("font-weight", "none");
+
+		scope.$watch('ngModel', function(model) {
+			var lbl = model.yLabel;
+			var data = model.data;
+			
+			console.log("Updating scatter with: " + data);
 			if(data) {
 				var maxX = d3.max(data, function(d) { return d.plots.length; });
 				// set x and y domains
 				x.domain(d3.range(maxX));
-				y.domain([300, d3.max(data, function(d) {return d3.max(d.plots, function(d) {return d.y}) + 100; })]);
+				
+				var yMax = d3.max(data, function(d) {return d3.max(d.plots, function(d) { return d.y; }); });
+				var yMin = d3.min(data, function(d){return d3.min(d.plots, function(d) { return d.y; }); });
+				var yDiff = yMax - yMin;
+				y.domain([yMin - yDiff *0.1, yMax + yDiff*0.1]);
 				
 				// render x and y axis
 				xRender.transition().duration(2000).call(xAxis).selectAll("text")
@@ -104,89 +132,140 @@ app.directive('scatterChart', function(){
 					.tickFormat("")
 				);
 				
-				// render axis labels
-				svg.append("text")
-					.attr("opacity", 0)
-					.attr("class", "x label")
-					.attr("text-anchor", "end")
-					.attr("x", width)
-					.attr("y", height - 6)
-					.text("Week - Day")
-					.transition().duration(2000).attr("opacity", 1);
-				svg.append("text")
-					.attr("opacity", 0)
-					.attr("class", "y label")
-					.attr("text-anchor", "end")
-					.attr("y", 6)
-					.attr("dy", ".75em")
-					.attr("transform", "rotate(-90)")
-					.text("elo rating")
-					.style("font-weight", "none")
-					.transition().duration(2000).attr("opacity", 1);
+				xLabel.text("Week - Day").transition().duration(2000).attr("opacity", 1);
+				yLabel.text(lbl).transition().duration(2000).transition().duration(2000).attr("opacity", 1);
 				
 				// add tooltip
 				var tooltip = d3.select(element[0]).append('div').attr('id', 'tooltip').style('opacity', 0);
 				
-				data.forEach(function(team, idx) {
-					// add pattern used for circle background
-					var pattern = defs.append('pattern')
-						.attr('id', team.id)
-						.attr('height', 22)
-						.attr('width', 22);
-					pattern.append('rect')
-						.attr('height', 22)
-						.attr('width', 22)
-						.attr('fill', 'skyblue');
-					pattern.append('image')
-						.attr('height', 22)
-						.attr('width', 22)
-						.attr('xlink:href', team.img);
-						
-					// add patternLarge used for circle background
-					var patternLarge = defs.append('pattern')
-						.attr('id', team.id + '-large')
-						.attr('height', 40)
-						.attr('width', 40);
-					patternLarge.append('rect')
-						.attr('height', 40)
-						.attr('width', 40)
-						.attr('fill', 'skyblue');
-					patternLarge.append('image')
-						.attr('height', 40)
-						.attr('width', 40)
-						.attr('xlink:href', team.img);
+				// functioning defining line
+				var line = d3.svg.line()
+					.x(function(d, i) { return x(i) + (width / maxX)/2; })
+					.y(function(d) { return y(d.y); })
+					.interpolate("monotone");
+				
+				// render line
+				lineContainer.selectAll('.team-line').data(data).exit().remove();
+				var currPath = lineContainer.selectAll('.team-line').data(data)
+					.attr('id', function(team) {
+						return team.id; })
+					.attr('fill', 'none')
+					.attr("class", "team-line")
+					.attr("d", function(team) {
+						return line(team.plots); })
+					.attr('pointer-events', 'none')
+					.style('stroke', function(d, i) { return colors[i]; })
+					.on("mouseover", function() {
+						d3.select(this).transition().duration(200).style("stroke-width", 5);
+					})
+					.on("mouseout", function() {
+						d3.select(this).transition().duration(200).style("stroke-width", 2);
+					});
 					
-					// functioning defining line
-					var line = d3.svg.line()
-						.x(function(d, i) { return x(i) + (width / maxX)/2; })
-						.y(function(d) { return y(d.y); })
-						.interpolate("monotone");
-					
-					// render line
-					var path = svg.append("path")
-						.attr('fill', 'none')
-						.attr("class", "line")
-						.attr("d", line(team.plots))
-						.attr('pointer-events', 'none')
-						.style('stroke', colors[idx])
-						.on("mouseover", function() {
-							d3.select(this).transition().duration(200).style("stroke-width", 5);
-						})
-						.on("mouseout", function() {
-							d3.select(this).transition().duration(200).style("stroke-width", 2);
-						});
-					var pathLength = path.node().getTotalLength();
-					
-					path.attr("stroke-dasharray", pathLength + " " + pathLength)
+				var path = lineContainer.selectAll('.team-line').data(data).enter().append("path")
+					.attr('id', function(team) {
+						return team.id; })
+					.attr('fill', 'none')
+					.attr("class", "team-line")
+					.attr("d", function(team) {
+						return line(team.plots); })
+					.attr('pointer-events', 'none')
+					.style('stroke', function(d, i) { return colors[i]; })
+					.on("mouseover", function() {
+						d3.select(this).transition().duration(200).style("stroke-width", 5);
+					})
+					.on("mouseout", function() {
+						d3.select(this).transition().duration(200).style("stroke-width", 2);
+					});
+				currPath.transition().delay(4000).attr('pointer-events', '');
+				path.transition().delay(4000).attr('pointer-events', '');
+				svg.selectAll('.team-line').each(function() {
+					var pathLength = d3.select(this).node().getTotalLength();
+					d3.select(this).attr("stroke-dasharray", pathLength + " " + pathLength)
 						.attr("stroke-dashoffset", pathLength)
 						.transition().delay(2000).duration(2000)
 						.attr("stroke-dashoffset", 0).attr('stroke-width', 2);
-
-					
+				});
+				
+				// add category selectors
+				var teamChoice = selectorContainer.selectAll('div').data(data, function(d) { return d.id; }).enter().append('div').attr('class', 'team').classed('select', true).style('pointer-events', 'none')
+					.on("mouseover", function(d) {
+						lineContainer.select('#' + d.id).style("stroke-width", 5);
+					})
+					.on("mouseout", function(d) {
+						lineContainer.select('#' + d.id).style("stroke-width", 2);
+					})
+					.on('click', function(d, i) {
+						// determines if selector is active or not
+						var active = true;
+						if(d3.select(this).classed('select')) { 
+							d3.select(this).classed('select', false).classed('deselect', true);
+							d3.select(this).select('.color-key').transition().duration(200).style('background', 'grey');
+							this.parentNode.appendChild(this);
+							lineContainer.select('#' + d.id).transition().attr('opacity', 0).attr('pointer-events', 'none');
+							plotContainer.select('#' + d.id).transition().attr('opacity', 0).attr('pointer-events', 'none');
+						}
+						else {
+							d3.select(this).classed('select', true).classed('deselect', false);
+							d3.select(this).select('.color-key').transition().style('background', colors[i]);
+							var firstChild = this.parentNode.firstChild;
+							this.parentNode.insertBefore(this, firstChild);
+							var thisLine = lineContainer.select('#' + d.id);
+							var thisLength = thisLine.node().getTotalLength();
+							
+							thisLine.attr("opacity", 1).style('stroke-width', 2)
+								.attr("stroke-dasharray", thisLength + " " + thisLength)
+								.attr("stroke-dashoffset", thisLength)
+								.transition().duration(1000)
+								.attr("stroke-dashoffset", 0);
+							plotContainer.selectAll('#' + d.id).transition().attr('opacity', 1).attr('pointer-events', '');
+						}
+					});
+				
+				teamChoice.append('div').attr('class', 'color-key').style('background', function(d,i) { return colors[i]; });
+				teamChoice.append('div').attr('class', 'img-container').append('img').attr('src', function(team) { return team.img; });
+				teamChoice.append('div').attr('class', 'info').append('span').text(function(team) { return team.id; });
+				teamChoice.transition().delay(4200).style('pointer-events', '');
+				
+				plotContainer.selectAll('g').data(data, function(d){ return d.id }).enter().append('g').attr('id', function(d) { return d.id; });
+				
+				// add pattern used for circle background
+				var patterns = defs.append('g').attr('class', 'patterns');
+				
+				var pattern = patterns.selectAll('pattern').data(data).enter().append('pattern')
+						.attr('id', function(team) { return team.id; })
+						.attr('height', 22)
+						.attr('width', 22);
+				pattern.append('rect')
+					.attr('height', 22)
+					.attr('width', 22)
+					.attr('fill', 'skyblue');
+				pattern.append('image')
+					.attr('height', 22)
+					.attr('width', 22)
+					.attr('xlink:href', function(team) { return team.img; });
+				
+				// add patternLarge used for circle background
+				var patternsLarge = defs.append('g').attr('class', 'patternsLarge');
+				var patternLarge = patternsLarge.selectAll('pattern').data(data).enter().append('pattern')
+					.attr('id', function(team) { return team.id + '-large'; })
+					.attr('height', 40)
+					.attr('width', 40);
+				patternLarge.append('rect')
+					.attr('height', 40)
+					.attr('width', 40)
+					.attr('fill', 'skyblue');
+				patternLarge.append('image')
+					.attr('height', 40)
+					.attr('width', 40)
+					.attr('xlink:href', function(team) { return team.img; });
+			
+				data.forEach(function(team, idx) {
 					// add/render plot points
-					var plt = svg.selectAll("dot")
-						.data(team.plots)
-						.enter().append("circle")
+					plotContainer.select('#'+ team.id).selectAll("circle")
+						.data(team.plots).exit().remove();
+					var currPlt = plotContainer.select('#'+ team.id).selectAll("circle")
+						.data(team.plots).attr('class', 'team-plots')
 							.attr("r", 11)
 							.attr("cx", function(d, i) { return x(i) + (width/maxX)/2; })
 							.attr("cy", function(d) { return y(d.y); })
@@ -194,69 +273,57 @@ app.directive('scatterChart', function(){
 							.attr("opacity", 0)
 							.attr('pointer-events', 'none')
 						.on("mouseover", function(d) {
-								d3.select(this).transition().duration(800).ease('elastic').attr('fill', 'url(#' + team.id + '-large)').attr('r', 20);
-								path.style("stroke-width", 5);
-								var xPos = parseFloat(d3.select(this).attr("cx"));
-								var yPos = parseFloat(d3.select(this).attr("cy"));
-								this.parentNode.appendChild(this);
-								tooltip.transition().duration(200).style("opacity", 1);		
-								tooltip.html(d.label + "<br/>" + (new Date(d.x).getMonth() +1) + "/" + new Date(d.x).getDate())	
-									.style("left", (xPos + 10) + "px")		
-									.style("top", (yPos - 17) + "px");
+							d3.select(this).transition().duration(800).ease('elastic').attr('fill', 'url(#' + team.id + '-large)').attr('r', 20);
+							lineContainer.select('#'+team.id+'.team-line').style("stroke-width", 5);
+							var xPos = parseFloat(d3.select(this).attr("cx"));
+							var yPos = parseFloat(d3.select(this).attr("cy"));
+							this.parentNode.appendChild(this);
+							tooltip.transition().duration(200).style("opacity", 1);		
+							tooltip.html(d.label + "<br/>" + (new Date(d.x).getMonth() +1) + "/" + new Date(d.x).getDate())	
+								.style("left", (xPos + 10) + "px")		
+								.style("top", (yPos - 17) + "px");
 						})
 						.on("mouseout", function() {
-							path.style("stroke-width", 2);
+							lineContainer.select('#'+team.id+'.team-line').style("stroke-width", 2);
 							d3.select(this).transition().attr('fill', 'url(#' + team.id + ')').attr('r', 11);
 							tooltip.transition().duration(200)		
 								.style("opacity", 0);
 						});
-					
-					plt.transition().duration(2000).delay(function(d,i) {return 2000+(1000/maxX)*i})
-							.attr('opacity', 0.8);
-					
-					// determines if selector is active or not
-					var active = true;
-					
-					// add category selectors
-					var teamChoice = selectorContainer.append('div').attr('class', 'team').classed('select', true).attr('height', 0).style('pointer-events', 'none')
-						.on("mouseover", function() {
-							path.style("stroke-width", 5);
+					var plt = plotContainer.select('#'+ team.id).selectAll("circle")
+						.data(team.plots)
+						.enter().append("circle")
+							.attr('class', 'team-plots')
+							.attr("r", 11)
+							.attr("cx", function(d, i) { return x(i) + (width/maxX)/2; })
+							.attr("cy", function(d) { return y(d.y); })
+							.attr("fill", "url(#"+ team.id +")")
+							.attr("opacity", 0)
+							.attr('pointer-events', 'none')
+						.on("mouseover", function(d) {
+							d3.select(this).transition().duration(800).ease('elastic').attr('fill', 'url(#' + team.id + '-large)').attr('r', 20);
+							lineContainer.select('#'+team.id+'.team-line').style("stroke-width", 5);
+							var xPos = parseFloat(d3.select(this).attr("cx"));
+							var yPos = parseFloat(d3.select(this).attr("cy"));
+							this.parentNode.appendChild(this);
+							tooltip.transition().duration(200).style("opacity", 1);		
+							tooltip.html(d.label + "<br/>" + (new Date(d.x).getMonth() +1) + "/" + new Date(d.x).getDate())	
+								.style("left", (xPos + 10) + "px")		
+								.style("top", (yPos - 17) + "px");
 						})
 						.on("mouseout", function() {
-							path.style("stroke-width", 2);
-						})
-						.on('click', function() {
-							if(active) { 
-								d3.select(this).classed('select', false).classed('deselect', true);
-								d3.select(this).select('.color-key').transition().duration(200).style('background', 'grey');
-								this.parentNode.appendChild(this);
-								path.transition().attr('opacity', 0).attr('pointer-events', 'none');
-								plt.transition().attr('opacity', 0).attr('pointer-events', 'none');
-								active = false;
-							}
-							else {
-								d3.select(this).classed('select', true).classed('deselect', false);
-								d3.select(this).select('.color-key').transition().style('background', colors[idx]);
-								var firstChild = this.parentNode.firstChild;
-								this.parentNode.insertBefore(this, firstChild);
-								path.attr("opacity", 1).style('stroke-width', 2)
-									.attr("stroke-dasharray", pathLength + " " + pathLength)
-									.attr("stroke-dashoffset", pathLength)
-									.transition().duration(1000)
-									.attr("stroke-dashoffset", 0);
-								plt.transition().attr('opacity', 1).attr('pointer-events', '');
-								active = true;
-							}
+							lineContainer.select('#'+team.id+'.team-line').style("stroke-width", 2);
+							d3.select(this).transition().attr('fill', 'url(#' + team.id + ')').attr('r', 11);
+							tooltip.transition().duration(200)		
+								.style("opacity", 0);
 						});
-					
-					teamChoice.append('div').attr('class', 'color-key').style('background', colors[idx]);
-					teamChoice.append('div').attr('class', 'img-container').append('img').attr('src', team.img);
-					teamChoice.append('div').attr('class', 'info').append('span').text(team.id);
-					
+						
+					currPlt.transition().duration(2000).delay(function(d,i) {return 2000+(1000/maxX)*i})
+							.attr('opacity', 0.8);
+					plt.transition().duration(2000).delay(function(d,i) {return 2000+(1000/maxX)*i})
+							.attr('opacity', 0.8);
 					// add in mouse events after load in
-					path.transition().delay(4000).attr('pointer-events', '');
+					currPlt.transition().delay(4200).attr('pointer-events', '');
 					plt.transition().delay(4200).attr('pointer-events', '');
-					teamChoice.transition().delay(4200).style('pointer-events', '');
 				});
 			}
 		}, true);
